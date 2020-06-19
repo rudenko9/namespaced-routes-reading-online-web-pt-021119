28fac5501a1f8990e5baa97916db27c957b3aaef
Nataliya R <n.rudenko@gmail.com>
2020-06-19 02:49:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
   resources :posts, only: %i[index show new create edit update]
 
  # get '/admin/stats', to: 'stats#index'
+<<<<<<< HEAD
  namespace :admin do 
+=======
+ scope '/admin', module: 'admin' do 
+>>>>>>> 8c351ee60e3077030b8f5de76800c1d56235e4ad
    resources :stats, only: [:index]
    
   get '/stats' , to: 'stats#index'
